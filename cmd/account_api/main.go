fc0b02443f23b06a7ac4b2d5474c9c5b5dbef891
Daniel Bahrami <daniel@bahrami.dk>
2025-09-30 13:44:28 +0200

    Set up main.go

--- cmd/account_api/main.go
+++ cmd/account_api/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/danielbahrami/account-api/internal/api"
+	"github.com/danielbahrami/account-api/internal/postgres"
+)
+
 func main() {
 
+	// Connect to Postgres
+	dbpool, err := postgres.ConnectPostgres()
+	if err != nil {
+		log.Fatalf("Failed to connect to Postgres: %v", err)
+	}
+	defer dbpool.Close()
+
+	// Create ServeMux
+	mux := http.NewServeMux()
+
+	// Setup API routes
+	api.SetupRoutes(mux, dbpool)
+
+	// Start the server on port 9090
+	if err := http.ListenAndServe(":9090", mux); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
