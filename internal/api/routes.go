97295a2df14062b37c9558f588e310f2b4fe9ba8
Daniel Bahrami <daniel@bahrami.dk>
2025-10-01 16:15:18 +0200

    Update endpoints

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// Registers all API endpoints
 func SetupRoutes(mux *http.ServeMux, dbpool *pgxpool.Pool) {
 	// Health endpoint
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -17,4 +18,39 @@ func SetupRoutes(mux *http.ServeMux, dbpool *pgxpool.Pool) {
 		w.Write([]byte("Ok"))
 	})
 
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+			return
+		}
+		loginHandler(w, r, dbpool)
+	})
+
+	// Accounts endpoints
+	mux.HandleFunc("/accounts", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			authMiddleware(func(w http.ResponseWriter, r *http.Request) {
+				listAccountsHandler(w, r, dbpool)
+			})(w, r)
+		case http.MethodPost:
+			authMiddleware(func(w http.ResponseWriter, r *http.Request) {
+				createAccountHandler(w, r, dbpool)
+			})(w, r)
+		default:
+			http.Error(w, "", http.StatusMethodNotAllowed)
+		}
+	})
+
+	// Transfer endpoint
+	mux.HandleFunc("/transfer", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			authMiddleware(func(w http.ResponseWriter, r *http.Request) {
+				transferHandler(w, r, dbpool)
+			})(w, r)
+		default:
+			http.Error(w, "", http.StatusMethodNotAllowed)
+		}
+	})
 }
